dee5d7fe479480d41dac8956bca950771c26b3f9
charan <charandeepnaga123@gmail.com>
2023-12-06 23:57:40 +0530

    linked list creation insertion deletion

--- linked-lists/linked_lists.py
+++ linked-lists/linked_lists.py
@@ -14,3 +14,29 @@ class LinkedList:
         else:
             new_node.next=self.head
             self.next=new_node
+
+    def insertionAtIndex(self,data,index):
+        new_node = Node(data)
+        position = 0
+        current_node = self.head
+        while(current_node!=None and position+1 != index):
+            position += 1
+            current_node= current_node.next
+        if current_node != None:
+            new_node.next = current_node.next
+            current_node.next = new_node
+        else:
+            print("Index Not Present.")
+
+    def insertionAtEnd(self,data,index):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current_node = self.head
+        
+        while(current_node.next!=None):
+            current_node.next = current_node
+        
+        current_node.next = new_node
